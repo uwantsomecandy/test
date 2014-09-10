7d3b8de7f8466c30f11d5d826c28ada56f4390c1
uwantsomecandy <djs_scheenloop@hotmail.com>
2014-09-10 11:28:22 +0200

    test2

--- test2.php
+++ test2.php
@@ -1,8 +1,10 @@
 <html>
 	<head>
+	<<meta charset="utf-8">
 		<title>hoi</title>
 	</head>
 	<body>
+		<h1>boom!	</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi 
 		accusantium tenetur, voluptatem asperiores quis iusto laboriosam, ab repudiandae numquam 
 		oluta doloremque molestiae voluptatibus dolore ratione. Dicta tempore, quae aliquid atque!</p>
